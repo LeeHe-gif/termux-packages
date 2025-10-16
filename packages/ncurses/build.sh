cdd346878dfc33ac64231d718a584b7094aa9d14
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-10-16 17:33:33 +0800

    Update build.sh

--- packages/ncurses/build.sh
+++ packages/ncurses/build.sh
@@ -69,7 +69,7 @@ termux_step_pre_configure() {
 	:
 }
 termux_step_post_make_install() {
-	cd /.termux-build/ncurses/massage/data/data/com.termux/files/usr/share/termuxinfo
+	cd ~/.termux-build/ncurses/massage/data/data/com.termux/files/usr/share/termuxinfo
 	zip -r termuxinfo.zip *
 	mv termuxinfo.zip ~
 }
